8e2721f08907a232c2da76dfedab5f8518a5e26b
Niamh Chaparro <Niamh.Chaparro@teck.com>
2021-04-17 14:25:59 -0700

    made some corrections

--- setup.py
+++ setup.py
@@ -5,15 +5,17 @@ with open('README.md', 'r') as file:
 
 
 setuptools.setup(
-	name = 'preprocess_nchaparr', # this should be unique globaLLy
-	version = '0.0.1',
+	name = 'preprocess_kgptalkie', #this should be unique
+	version = '0.0.3',
 	author = 'Laxmi Kant',
 	author_email = 'udemy@kgptalkie.com',
 	description = 'This is preprocessing package',
 	long_description = long_description,
-	long_description_content_type='text/markdown',
-	packages=setuptools.find_packages(),
-	classifiers = ['Programming Language :: Python :: 3',
-	'License :: OSI Approved :: MIT License'. "Operating System :: OS Independent"],
+	long_description_content_type = 'text/markdown',
+	packages = setuptools.find_packages(),
+	classifiers = [
+	'Programming Language :: Python :: 3',
+	'License :: OSI Aproved :: MIT License',
+	"Operating System :: OS Independent"],
 	python_requires = '>=3.5'
 	)
