cfdfbd3c86db3e8bdbb716504b8366f21e304fce
Niamh Chaparro <Niamh.Chaparro@teck.com>
2021-05-08 11:54:47 -0700

    made some corrections

--- preprocess_kgptalkie/__init__.py
+++ preprocess_kgptalkie/__init__.py
@@ -41,8 +41,10 @@ def remove_urls(x):
 def remove_emails(x):
 	return utils._remove_emails(x)
 
+def remove_rt_old(x):
+	return utils._remove_rt(x)
+
 def remove_rt(x):
-#correction test
 	return utils._remove_rt(x)
 
 def remove_special_chars(x):
